6213d924796f04b5e73618b71921a1f7c5c5fecf
stephane cooperstein <sbcooperstei@wisc.edu>
2015-11-19 09:17:29 -0600

    updated to first full 13 TeV BDT training

--- VHbbAnalysis/RunAnalysis.py
+++ VHbbAnalysis/RunAnalysis.py
@@ -65,7 +65,7 @@ else:
             content += "Log    = %i.log\n"    % nFiles
             content += "Notification = never\n"
             content += "WhenToTransferOutput=On_Exit\n"
-            content += "transfer_input_files = ../../%s,../../cfg/samples.txt,../../cfg/earlybranches.txt,../../cfg/existingbranches.txt,../../cfg/newbranches.txt,../../cfg/bdtsettings.txt,../../cfg/reg1_settings.txt,../../cfg/reg2_settings.txt,../../cfg/settings.txt,../../aux/new-weights-23Jan.xml,../../aux/TEST_TMVA_13TeV_H125Sig_0b1b2bWjetsTTbarBkg_Mjj_BDT.weights.xml,../../RunSample.py,../../../AnalysisDict.so\n" % options.configFile
+            content += "transfer_input_files = ../../%s,../../cfg/samples.txt,../../cfg/earlybranches.txt,../../cfg/existingbranches.txt,../../cfg/newbranches.txt,../../cfg/bdtsettings.txt,../../cfg/reg1_settings.txt,../../cfg/reg2_settings.txt,../../cfg/settings.txt,../../aux/new-weights-23Jan.xml,../../aux/TMVA_13TeV_H125Sig_0b1b2bWjetsTTbarBkg_Mjj_BDT.weights.xml,../../RunSample.py,../../../AnalysisDict.so\n" % options.configFile
             content += "Queue = 1\n"
             print content
             submitFile.write(content)
