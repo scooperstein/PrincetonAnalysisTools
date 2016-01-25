c1b328442417c6977f0c039981d91a3db0e437ea
stephane cooperstein <sbcooperstei@wisc.edu>
2016-01-25 11:00:15 -0600

    add multiple BDT's at a time and plot them

--- scripts/addBDTToTree.C
+++ scripts/addBDTToTree.C
@@ -1,4 +1,4 @@
-void addBDTToTree(const char* oldfilename="testingtree_mh125_evenbkg_newmvas.root",const char* newfilename="testingtree_mh125_evenbkg_allmvas.root", const char* weights="weights/TMVA_vbf_dijet_diphosherpa_BDT_evenb_fixwt_Gradient.weights.xml",const char* newbranch="combined_bdt",bool refill=false) {
+void addBDTToTree(const char* oldfilename="testingtree_mh125_evenbkg_newmvas.root",const char* newfilename="testingtree_mh125_evenbkg_allmvas.root", std::vector<const char*> weights=std::vector<const char*>(),std::vector<const char*> newbranch=std::vector<const char*>(),bool refill=false) {
   
 #include <string>
 
@@ -6,35 +6,38 @@ void addBDTToTree(const char* oldfilename="testingtree_mh125_evenbkg_newmvas.roo
   using std::max;
   using std::min;
   
-  TMVA::Reader  *tmvaReader_BDT; 
+  int nBDTs = (int) weights.size();
+  std::vector<TMVA::Reader*>  tmvaReader_BDTs; 
 
   Float_t   H_mass, H_pt, V_pt, hJets_btagCSV_0, hJets_btagCSV_1, HVdPhi, nAddJet_f, nAddLep_f, hJets_pt_0, hJets_pt_1, met_pt, Top1_mass_fromLepton, lepMetDPhi, selLeptons_pt_0, selLeptons_eta_0, selLeptons_relIso03_0, isWenu_f, isWmunu_f;
 
   Int_t isWenu;
-  Float_t BDT;
+  Float_t BDT[nBDTs];
 
-  tmvaReader_BDT = new TMVA::Reader("V:!Color:Silent");
-  tmvaReader_BDT->AddVariable("H_mass",&H_mass);
-  tmvaReader_BDT->AddVariable("H_pt", &H_pt);
-  tmvaReader_BDT->AddVariable("V_pt", &V_pt);
-  tmvaReader_BDT->AddVariable("Jet_btagCSV[hJetInd1]", &hJets_btagCSV_0);
-  tmvaReader_BDT->AddVariable("Jet_btagCSV[hJetInd2]", &hJets_btagCSV_1);
-  tmvaReader_BDT->AddVariable("HVdPhi", &HVdPhi);
-  //tmvaReader_BDT->AddVariable("nAddJet_f", &nAddJet_f);
-  //tmvaReader_BDT->AddVariable("nAddLep_f", &nAddLep_f);
-  tmvaReader_BDT->AddVariable("Jet_pt[hJetInd1]", &hJets_pt_0);
-  tmvaReader_BDT->AddVariable("Jet_pt[hJetInd2]", &hJets_pt_1);
-  tmvaReader_BDT->AddVariable("met_pt", &met_pt);
-  tmvaReader_BDT->AddVariable("Top1_mass_fromLepton", &Top1_mass_fromLepton);
-  tmvaReader_BDT->AddVariable("lepMetDPhi", &lepMetDPhi);
-  tmvaReader_BDT->AddVariable("selLeptons_pt[lepInd]", &selLeptons_pt_0);
-  tmvaReader_BDT->AddVariable("selLeptons_eta[lepInd]", &selLeptons_eta_0);
-  tmvaReader_BDT->AddVariable("selLeptons_relIso03[lepInd]", &selLeptons_relIso03_0);
-  tmvaReader_BDT->AddVariable("isWenu", &isWenu_f);
-  tmvaReader_BDT->AddVariable("isWmunu_f", &isWmunu_f);
+  for (int i=0; i<nBDTs; i++) {
+      tmvaReader_BDTs.push_back(new TMVA::Reader("V:!Color:Silent"));
+      tmvaReader_BDTs[i]->AddVariable("H_mass",&H_mass);
+      tmvaReader_BDTs[i]->AddVariable("H_pt", &H_pt);
+      tmvaReader_BDTs[i]->AddVariable("V_pt", &V_pt);
+      tmvaReader_BDTs[i]->AddVariable("Jet_btagCSV[hJetInd1]", &hJets_btagCSV_0);
+      tmvaReader_BDTs[i]->AddVariable("Jet_btagCSV[hJetInd2]", &hJets_btagCSV_1);
+      tmvaReader_BDTs[i]->AddVariable("HVdPhi", &HVdPhi);
+      tmvaReader_BDTs[i]->AddVariable("nAddJet_f", &nAddJet_f);
+      tmvaReader_BDTs[i]->AddVariable("nAddLep_f", &nAddLep_f);
+      tmvaReader_BDTs[i]->AddVariable("Jet_pt[hJetInd1]", &hJets_pt_0);
+      tmvaReader_BDTs[i]->AddVariable("Jet_pt[hJetInd2]", &hJets_pt_1);
+      tmvaReader_BDTs[i]->AddVariable("met_pt", &met_pt);
+      tmvaReader_BDTs[i]->AddVariable("Top1_mass_fromLepton", &Top1_mass_fromLepton);
+      tmvaReader_BDTs[i]->AddVariable("lepMetDPhi", &lepMetDPhi);
+      tmvaReader_BDTs[i]->AddVariable("selLeptons_pt[lepInd]", &selLeptons_pt_0);
+      tmvaReader_BDTs[i]->AddVariable("selLeptons_eta[lepInd]", &selLeptons_eta_0);
+      tmvaReader_BDTs[i]->AddVariable("selLeptons_relIso03[lepInd]", &selLeptons_relIso03_0);
+      tmvaReader_BDTs[i]->AddVariable("isWenu", &isWenu_f);
+      tmvaReader_BDTs[i]->AddVariable("isWmunu_f", &isWmunu_f);
 
-  tmvaReader_BDT->BookMVA("BDT", weights);
-  cout<<"finished tmvaReader_BDT"<<endl;
+      tmvaReader_BDTs[i]->BookMVA("BDT", weights[i]);
+      cout<<"finished tmvaReader_BDT "<<i<<endl;
+  }
 
   TFile *oldfile = new TFile(oldfilename);
   
@@ -63,15 +66,17 @@ void addBDTToTree(const char* oldfilename="testingtree_mh125_evenbkg_newmvas.roo
   TFile *newfile = new TFile(newfilename, "recreate");
   TTree *newtree = oldtree->CloneTree(0);
 
-  TBranch *bBDT=0;
-  if(refill){
-    oldtree->SetBranchAddress(newbranch,  &BDT);
-  } else {  
-    char nametype[100];
-    strcpy(nametype,newbranch);
-    strcat(nametype,"/F");
+  std::vector<TBranch*> bBDT;
+  for (int i=0; i<nBDTs; i++) {
+    if(refill){
+      oldtree->SetBranchAddress(newbranch[i],  &BDT[i]);
+    } else {  
+      char nametype[100];
+      strcpy(nametype,newbranch[i]);
+      strcat(nametype,"/F");
     
-    bBDT = newtree->Branch(newbranch,&BDT);
+      bBDT.push_back(newtree->Branch(newbranch[i],&BDT[i]));
+    }
   }
 
   cout<<"starting loop over "<<nentries<<" events"<<endl;
@@ -79,10 +84,12 @@ void addBDTToTree(const char* oldfilename="testingtree_mh125_evenbkg_newmvas.roo
     oldtree->GetEntry(z);
     isWenu_f = (float) isWenu;
 
-    //BDT  = tmvaReader_BDT->EvaluateMVA("Gradient");       
-    BDT  = tmvaReader_BDT->EvaluateMVA("BDT");       
+    //BDT  = tmvaReader_BDT->EvaluateMVA("Gradient");
+    for (int i=0; i<nBDTs; i++) {       
+        BDT[i]  = tmvaReader_BDTs[i]->EvaluateMVA("BDT");       
 
-    if(z%50000==1) cout<<z<<"  "<<BDT<<std::endl;
+        if(z%50000==1) cout<<z<<"  "<<BDT[i]<<std::endl;
+    }
     /*std::cout<<"H_mass = "<<H_mass<<std::endl;
     std::cout<<"H_pt = "<<H_pt<<std::endl;
     std::cout<<"V_pt = "<<V_pt<<std::endl;
