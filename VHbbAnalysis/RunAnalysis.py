5efe0b7471cd15185bc3fd5b510f1bf215458482
Stephane Cooperstein <stephane.b.cooperstein@cern.ch>
2017-11-06 11:46:14 +0100

    one small fix for lxplus running

--- VHbbAnalysis/RunAnalysis.py
+++ VHbbAnalysis/RunAnalysis.py
@@ -131,6 +131,7 @@ else:
                 #content += "transfer_input_files = ../../%s,../../cfg/samples.txt,../../cfg/earlybranches.txt,../../cfg/existingbranches.txt,../../cfg/newbranches.txt,../../cfg/bdtsettings.txt,../../cfg/reg1_settings.txt,../../cfg/reg2_settings.txt,../../cfg/settings.txt,../../aux/TMVARegression_BDTG_ttbar_Nov23.weights.xml,../../aux/TMVA_13TeV_Dec14_3000_5_H125Sig_0b1b2bWjetsTTbarBkg_Mjj_BDT.weights.xml,../../aux/MuonIso_Z_RunCD_Reco74X_Dec1.json,../../aux/SingleMuonTrigger_Z_RunCD_Reco74X_Dec1.json,../../aux/MuonID_Z_RunCD_Reco74X_Dec1.json,../../aux/CutBasedID_TightWP.json,../../aux/CutBasedID_LooseWP.json,../../RunSample.py,../../../AnalysisDict.so,../../cfg/systematics.txt,../../cfg/scalefactors.txt\n" % options.configFile
                 content += "transfer_input_files = ../../%s,../../RunSkim.py,../../RunSample.py,../../../AnalysisDict.so,../../../env.sh,../../cfg,../../aux,../../../python/ReadInput.py,../../../AnalysisDict_rdict.pcm,../../../HelperClasses,../../../AnalysisManager.h,../../../plugins\n" % options.configFile
                 content += "Queue  1\n"
+            else:
                 fname = "%s/%s/job%iSubmit.sh" % (jobName, sampleName,nProcJobs)
                 submitFile = open(fname, "w")
                 content = "source %s/%s/condor_runscript.sh %s %s %s output_%s_%i.root\n" % (os.getcwd(),jobName,options.configFile, sampleName, filesToRun,sampleName, nProcJobs)  
