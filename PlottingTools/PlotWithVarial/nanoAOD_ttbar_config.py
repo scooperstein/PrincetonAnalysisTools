e7cd40812e355b783bd4e039bcb91855a2f8330d
HeinerTholen <heiner.tholen@cern.ch>
2018-02-18 20:12:06 +0100

    Feature plotting tools (#153)
    * load config from anywhere // allow different TreeProjectors
    
    * FIX in config: cutstring for muon pt
    
    * N-1 plots (latest Varial is needed)

--- PlottingTools/PlotWithVarial/nanoAOD_ttbar_config.py
+++ PlottingTools/PlotWithVarial/nanoAOD_ttbar_config.py
@@ -1,43 +1,57 @@
 
-name = 'VHbbPlotsSync'
+name = 'VHbbPlotsNanoAOD'
 input_pattern = '/eos/cms/store/user/scoopers/VHbbPostNano2017_V1/%s/*/*/*/*.root'
 weight = '1'
 enable_reuse_step = True  # try to find output on disk and don't run a step if present
 treename = 'Events'
 
 the_samples_dict = {
-#   name                     index  scale  legend      input tokens (sample name in the previous step)
-    'TT_powheg':            [None,  1.,    'Top pair', ['TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8']],
+#   name           index  scale             legend      input tokens (goes into input_pattern)
+    #'TT_powheg':  [None,  13245./35653.,    'Top pair', ['TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8']],
+    'TT_powheg':  [None,  43714./50265.,    'Top pair', ['TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8']],
+    'Data_Mu':    [None,  1.,               'Data',     ['SingleMuon']],
+    # 'Data_El':    [None,  1.,               'Data',     ['DoubleEG']],
 }
 
 
 plot_vars = {
-    'count':        ('1',           ';;event count',    1,  .5, 1.5),
-    'puWeight':     ('puWeight',    ';puWeight;',       60, 0,  1.5),
-    'Hpt':          ('H_pt',        ';p_{T}(jj)@GeV;',  21, 0,  350),
-    'Vpt':          ('V_pt',        ';p_{T}(W)@[GeV];', 30, 0,  500),
+    'Hpt':      ('H_pt',                    ';p_{T}(jj) [GeV];',                    21, 0,      350 ),
+    'Vpt':      ('V_pt',                    ';p_{T}(V) [GeV];',                     30, 0,      500 ),
+    'mjj':      ('H_mass',                  ';m(jj) [GeV];',                        17, 0,      255 ),
+    'CMVAmax':  ('Jet_btagCMVA[hJidx[0]]',  ';CMVA_{max};',                         20, -1,     1   ),
+    'CMVAmin':  ('Jet_btagCMVA[hJidx[1]]',  ';CMVA_{min};',                         20, -1,     1   ),
+    'jjWPtBal': ('H_pt/V_pt',               ';H/V p_{T} balance;',                  25, 0,      2.  ),
+    # 'HVdPhi':   ('HVdPhi',                  ';HVdPhi [rad];',                       30, -3.2,   3.2 ),
+    # 'drjj':     ('HJ1_HJ2_dR',              ';reg. Delta R(jj);',                   30, 0,      6   ),
 }
 
 
 categories_CR_2Lepton_TTbar_lowVpt = {
-    'CR_2Lepton_lowVpt_TTbar_Mu': '(Vtype == 0 && V_pt >= 50 && V_pt < 150)',
-    'CR_2Lepton_lowVpt_TTbar_El': '(Vtype == 1 && V_pt >= 50 && V_pt < 150)',
+    'CR_2Lepton_lowVpt_TTbar_Mu': ['Vtype == 0', 'V_pt >= 50 && V_pt < 150'],
+#    'CR_2Lepton_lowVpt_TTbar_El': '(Vtype == 1 && V_pt >= 50 && V_pt < 150)',
 }
 
 sel_CR_2Lepton_TTbar = [
-    '(HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL || HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL_DZ || HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ)',
+    #'(HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL || HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL_DZ || HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ)',
+    #'(HLT_IsoMu27 || HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ)',
+    'HLT_IsoMu27',
+    '(HLT_IsoMu27 ? Muon_pt[0] > 30 : 1)',
     'nJet >= 2',
-    'jet_Pt[0] > 20',
-    'jet_Pt[1] > 20',
+    'Jet_Pt[hJidx[0]] > 20',
+    'Jet_Pt[hJidx[1]] > 20',
     '(V_mass > 10 && (V_mass < 75 || V_mass > 120))',
-    '(Jet_btagCMVA[0] > Jet_btagCMVA[1] ? Jet_btagCMVA[0] : Jet_btagCMVA[1]) > 0.9432',  # CMVAmax > CMVAT
-    '(Jet_btagCMVA[0] > Jet_btagCMVA[1] ? Jet_btagCMVA[1] : Jet_btagCMVA[0]) > -0.5884', # CMVAmin > CMVAL
+    'Jet_btagCMVA[hJidx[0]] > 0.9432',  # CMVAmax > CMVAT
+    'Jet_btagCMVA[hJidx[1]] > -0.5884', # CMVAmin > CMVAL
 ]
 
 the_category_dict = {
-#   region block     categories                             sel.                     histograms
-    'TTbar':         [categories_CR_2Lepton_TTbar_lowVpt,   sel_CR_2Lepton_TTbar,    plot_vars],
+#   region block     categories                            sel.                     histograms
+    'TTbarCR':      [categories_CR_2Lepton_TTbar_lowVpt,   sel_CR_2Lepton_TTbar,    plot_vars],
+    'TTbarCR_N-1':  [categories_CR_2Lepton_TTbar_lowVpt,   sel_CR_2Lepton_TTbar,    plot_vars],
 }
 
 
 sample_colors = {'Top pair': 600}
+
+from varial_ext.treeprojector import TreeProjectorFileBased
+TreeProjector = TreeProjectorFileBased
--- PlottingTools/PlotWithVarial/run_plot_from_tree.py
+++ PlottingTools/PlotWithVarial/run_plot_from_tree.py
@@ -8,7 +8,9 @@ except ImportError:
     exit(-1)
 
 from varial_ext.treeprojector import TreeProjector
+from varial.util import iterableize
 import collections
+import itertools
 import glob
 import sys
 import os
@@ -18,12 +20,19 @@ if len(sys.argv) < 2:
     exit(-1)
 
 sys.dont_write_bytecode = True
+
+cwd = os.getcwd()
+full_cfg_path = sys.argv[1]
+cfg_path, cfg = os.path.split(full_cfg_path.replace('.py', ''))
 try:
-    config = __import__(sys.argv[1].replace('.py', ''))
+    os.chdir(cfg_path)
+    config = __import__(cfg)
 except ImportError, e:
     print 'Could not load config. Exit.'
     print e
     exit(-1)
+finally:
+    os.chdir(cwd)
 
 # use ~80% of cores ...
 varial.settings.max_num_processes = int(varial.settings.max_num_processes * 0.8) + 1
@@ -59,19 +68,16 @@ filenames = collections.OrderedDict(
     for s_name in ordered_s_names
 )
 
-def make_weight_string(index, scale):
+def make_weight_string(index):
     if index == 0:  # Data
         return '(sampleIndex==0)'
     if index != None:
-        return config.weight + '*(sampleIndex==%i)*%g' % (index, scale)
-    return config.weight + '*%g' % scale
+        return config.weight + '*(sampleIndex==%i)' % index
+    return config.weight
 
 # make sample weights_dict
 weights_dict = dict(
-    (
-        s_name,
-        make_weight_string(s_items[0], s_items[1])
-    )
+    (s_name, make_weight_string(s_items[0]))
     for s_name, s_items in config.the_samples_dict.iteritems()
 )
 # structure of weights_dict: {'samplename': 'weight*(sampleIndex==12345)*scale', ...}
@@ -83,8 +89,8 @@ def mk_tree_projector(region_block):
     # plug a block of parameter together (including histogram def's)
     params = {
         'histos': histos,
-        'treename': 'tree',
-        'nm1': False,  # don't make N-1 plots for now
+        'treename': getattr(config, 'treename', 'tree'),
+        'nm1': region_block.endswith('N-1'),
     }
     params.update(getattr(config, 'params', {}))
 
@@ -93,17 +99,18 @@ def mk_tree_projector(region_block):
         base_sel = []
 
     # define selections to be run with (section name, selection list, weights)
+    nm1_token = '_N-1' if params['nm1'] else ''
     sec_sel_weight = list(
-        (cat_name, [cat_sel] + base_sel, weights_dict)
+        (cat_name+nm1_token, list(iterableize(cat_sel))+base_sel, weights_dict)
         for cat_name, cat_sel in cats.iteritems()
     )
 
     # create and feed the tool for the plot-from-tree step
-    return TreeProjector(
+    TP_class = getattr(config, 'TreeProjector', TreeProjector)
+    return TP_class(
         filenames, params, sec_sel_weight,
         name=region_block,
         add_aliases_to_analysis=False,
-        proc_sections_in_parallel=False,
     )
 
 treeplotters = varial.tools.ToolChainParallel(
@@ -111,6 +118,17 @@ treeplotters = varial.tools.ToolChainParallel(
     list(mk_tree_projector(block) for block in config.the_category_dict),
     n_workers=getattr(config, 'n_parallel_treeplotters', 1),
 )
+
+# protect against category override:
+pltr_combos = itertools.combinations(treeplotters.tool_chain, 2)
+for pltr1, pltr2 in pltr_combos:
+    sections1 = set(s for s, _, _ in pltr1.sec_sel_weight)
+    sections2 = set(s for s, _, _ in pltr2.sec_sel_weight)
+    intersect = sections1 & sections2
+    if intersect:
+        raise RuntimeError(
+            'In config "%s": the blocks "%s" and "%s" have common sections: %s' % (
+                full_cfg_path, pltr1.name, pltr2.name, intersect))
 
 # plotting...
 varial.settings.colors.update(config.sample_colors)
@@ -123,10 +141,19 @@ def input_hook(wrps):
     )
     wrps = varial.gen.gen_add_wrp_info(
         wrps,
-        is_data = lambda w: w.sample == 'Data',
+        is_data = lambda w: config.the_samples_dict[w.sample][2] == 'Data', # legend == 'Data'?
         is_signal = lambda w: 'H125' in w.sample,
         legend = lambda w: config.the_samples_dict[w.sample][2],
+        scale = lambda w: config.the_samples_dict[w.sample][1],
     )
+
+    # scale histograms if the scale is different from 1.
+    @varial.history.track_history
+    def scale(wrp):
+        wrp.obj.Scale(wrp.scale)
+        return wrp
+
+    wrps = varial.gen.imap_conditional(wrps, lambda w: w.scale != 1., scale)
 
     # the cutFlow histogram should show the integral of events up to a certain step
     wrps = varial.gen.imap_conditional(
